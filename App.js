23c11e03a05289933286220d69f61d41aa3fe431
Long Luu <luuhoanglong577@gmail.com>
2022-01-05 21:20:29 +0700

    button + alert

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
-import { StyleSheet, SafeAreaView, Button } from "react-native";
+import { StyleSheet, SafeAreaView, Button, Alert } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Button color="orange" title="click me" onPress={() => alert("button")} />
+      <Button
+        color="orange"
+        title="click me"
+        onPress={() =>
+          Alert.alert("my text", "my message", [
+            { text: "Yes", onPress: () => console.log("yes") },
+            { text: "No", onPress: () => console.log("no") },
+          ])
+        }
+      />
     </SafeAreaView>
   );
 }
