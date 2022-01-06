7060769bb002bb4125dc8ba7bedbb90c9babea00
Long Luu <luuhoanglong577@gmail.com>
2022-01-06 13:47:09 +0700

    add image

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { ImageBackground, StyleSheet, View } from "react-native";
+import { Button, ImageBackground, StyleSheet, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground
-        source={require("./assets/background.png") resizeMode="cover" style={styles.image}}
-      ></ImageBackground>
+        source={require("./assets/background.png")}
+        style={styles.image}
+      >
+        <Image source={require("./assets/logo-red.png")} style={styles.logo} />
+        <View style={styles.bottomButton}>
+          <Button title="Sign In" color={"#fc5c65"} />
+          <Button title="Sign Up" color={"#4ECDC4"} />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
   },
+  logo: {
+    flexDirection: "row",
+    width: 70,
+    height: 70,
+    alignSelf: "center",
+    top: 30,
+  },
   image: {
     flex: 1,
-    justifyContent: "center"
+    justifyContent: "center",
+  },
+  bottomButton: {
+    flex: 1,
+    justifyContent: "flex-end",
   },
 });
