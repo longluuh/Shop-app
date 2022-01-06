4eb83cc2731c7c4fa7d4c55f5760b7f46dbe67f1
Long Luu <luuhoanglong577@gmail.com>
2022-01-06 11:03:00 +0700

    inital project

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
-import { View } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("./assets/background.png") resizeMode="cover" style={styles.image}}
+      ></ImageBackground>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+});
