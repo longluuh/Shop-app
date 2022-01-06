6894e8144235647e3503b1fe80d5662ddb950dd0
Long Luu <luuhoanglong577@gmail.com>
2022-01-06 15:40:15 +0700

    rewrite ViewImageScreen

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -4,28 +4,40 @@ import { Button, View, StyleSheet, Image } from "react-native";
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
-      <View style={styles.button}>
-        <Button title="on" color={"#fc5c65"} />
-        <Button title="off" color={"#4ECDC4"} />
-      </View>
-      <Image style={styles.image} source={require("../assets/chair.png")} />
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/chair.png")}
+      />
+      <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
-  },
-  button: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    top: 30,
+    backgroundColor: "#000",
   },
   image: {
-    top: "10%",
     width: "100%",
-    height: "70%",
+    height: "100%",
+  },
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#fc5c65",
+    position: "absolute",
+    top: 20,
+    left: 50,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#4ECDC4",
+    position: "absolute",
+    top: 20,
+    right: 50,
   },
 });
 
