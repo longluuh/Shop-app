8e8e2e83d61a9acbc288bb9fae9812abd49eed64
Long Luu <luuhoanglong577@gmail.com>
2022-01-06 15:17:34 +0700

    update wellcomeScreen

--- app/screens/WellcomeScreen.js
+++ app/screens/WellcomeScreen.js
@@ -1,42 +1,55 @@
 import react from "react";
-import { Button, ImageBackground, StyleSheet, View, Image } from "react-native";
+import {
+  Button,
+  ImageBackground,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+} from "react-native";
 
 function WelcomeScreen() {
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require("../assets/background.png")}
-        style={styles.image}
-      >
-        <Image source={require("../assets/logo-red.png")} style={styles.logo} />
-        <View style={styles.bottomButton}>
-          <Button title="Sign In" color={"#fc5c65"} />
-          <Button title="Sign Up" color={"#4ECDC4"} />
-        </View>
-      </ImageBackground>
-    </View>
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.png")}
+    >
+      <View style={styles.logoContent}>
+        <Image
+          style={styles.logo}
+          source={require("../assets/logo-red.png")}
+        ></Image>
+        <Text> want you to sell</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  logo: {
-    flexDirection: "row",
-    width: 70,
-    height: 70,
-    alignSelf: "center",
-    top: 30,
-  },
-  image: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  bottomButton: {
+  background: {
     flex: 1,
     justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContent: {
+    position: "absolute",
+    top: 70,
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ECDC4",
   },
 });
 
